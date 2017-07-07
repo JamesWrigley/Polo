9d8350ecdf4bec2e49e47a086898799311356e97
JamesWrigley <james@puiterwijk.org>
2017-07-08 04:15:28 +1200

    Add the ability to choose images to display

--- Polo.py
+++ Polo.py
@@ -1,13 +1,14 @@
 import sys
 
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtSvg import QSvgWidget
-from PyQt5.QtWidgets import (QApplication, QDesktopWidget, QLabel, QMessageBox,
-                             QPushButton, QHBoxLayout, QVBoxLayout, QWidget)
+from PyQt5.QtWidgets import (QApplication, QDesktopWidget, QLabel, QFileDialog, QMessageBox,
+                             QPushButton, QHBoxLayout, QVBoxLayout, QWidget, QStackedWidget)
 
 
 class Polo(QWidget):
-    media_preview = None
+    media_preview_stack = None
     display_widget = None
 
     def __init__(self):
@@ -21,10 +22,16 @@ class Polo(QWidget):
 
         # Create widgets
         self.display_widget = QLabel()
-        self.media_preview = QSvgWidget("blank.svg")
+        self.media_preview_stack = QStackedWidget()
         open_button = QPushButton("Open")
         clear_button = QPushButton("Clear")
 
+        self.media_preview_stack.addWidget(QSvgWidget("blank.svg"))
+        self.media_preview_stack.addWidget(QLabel())
+        self.display_widget.setScaledContents(True)
+        self.display_widget.setStyleSheet("background-color: rgb(20, 20, 20);")
+        open_button.clicked.connect(self.choose_media)
+        clear_button.clicked.connect(self.clear_media)
         open_button.setToolTip("Choose a media file to display")
         clear_button.setToolTip("Clear the current media and turn off the display")
 
@@ -38,7 +45,7 @@ class Polo(QWidget):
         vbox.addStretch()
 
         hbox.addLayout(vbox)
-        hbox.addWidget(self.media_preview)
+        hbox.addWidget(self.media_preview_stack)
 
         hbox.setSpacing(30)
         hbox.setContentsMargins(20, 20, 20, 20)
@@ -59,11 +66,26 @@ class Polo(QWidget):
         self.setWindowTitle("Polo")
         self.show()
 
+    def choose_media(self):
+        media = QFileDialog.getOpenFileName(self, "Select Media")
+        if media:
+            self.display_widget.setPixmap(QPixmap(media[0]))
+            preview_label = self.media_preview_stack.widget(1)
+            preview_label.setStyleSheet("border-image: url({0})".format(media[0]))
+            self.media_preview_stack.setCurrentIndex(1)
+
+    def clear_media(self):
+        self.display_widget.clear()
+        self.media_preview_stack.setCurrentIndex(0)
+
     def center_widget(self, widget, screen):
         frame = widget.frameGeometry()
         screen_center = QDesktopWidget().availableGeometry(screen).center()
         frame.moveCenter(screen_center)
         widget.move(frame.topLeft())
+
+    def closeEvent(self, event):
+        self.display_widget.close()
 
 
 if __name__ == "__main__":
