1c9a6dc35e899b56a412d1b504d162cd4569e48a
JamesWrigley <james@puiterwijk.org>
2017-07-20 13:21:31 +1200

    Add the ability to cycle through all media files in the chosen file directory

--- Polo.py
+++ Polo.py
@@ -1,20 +1,23 @@
 import sys
+import glob
 import time
 import queue
 import imageio
 import argparse
 import threading
 
+from pathlib import Path
 from PIL.ImageQt import ImageQt
 from PIL import Image, ImageOps, ImageFile
 
 from PyQt5.QtSvg import QSvgWidget
 from PyQt5.QtCore import pyqtSignal, Qt
-from PyQt5.QtGui import QKeySequence, QPixmap
+from PyQt5.QtGui import QKeySequence, QPixmap, QIcon
 from PyQt5.QtWidgets import (QApplication, QCheckBox, QDesktopWidget, QLabel,
                              QLineEdit, QFileDialog, QLayout, QMessageBox,
                              QPushButton, QHBoxLayout, QVBoxLayout, QWidget,
                              QShortcut, QSizePolicy, QStackedWidget)
+
 
 # A video type alias for convenience
 Video = imageio.plugins.ffmpeg.FfmpegFormat.Reader
@@ -39,11 +42,20 @@ class DisplayWidget(QLabel):
         self.closed.emit()
 
 class Polo(QWidget):
+    # The media files in the current media directory
+    files = []
+    current_file_index = -1
+
     # The input media
     media = None
 
-    # The hologrified media as a Qt-compatible object (i.e. ImageQt)
+    # The hologrified media and preview media as Qt-compatible objects
+    # (i.e. ImageQt objects). We need to store these because they get converted
+    # to QPixmap's, which only store references to the original image. So if
+    # these weren't stored, Pythons garbage collector would delete them and
+    # cause Polo to segfault.
     qmedia = None
+    preview_qmedia = None
 
     # The current frame if the input media is a video
     current_frame = -1
@@ -65,9 +77,18 @@ class Polo(QWidget):
     # QLabel that displays the users selected media.
     media_preview_stack = None
 
+    # Widget that holds the navigation widgets (next/previous buttons)
+    nav_widget = None
+    next_shortcut = None
+    previous_shortcut = None
+
     # Screen dimension widgets
     size_widget = None
     size_checkbox = None
+
+    # Supported media formats
+    image_fmts = ["bmp", "gif", "jpeg", "jpg", "png", "ppm"]
+    video_fmts = ["avi", "mkv", "mov", "mp4", "mpg", "mpeg"]
 
     def __init__(self):
         """
@@ -88,8 +109,11 @@ class Polo(QWidget):
         self.media_preview_stack = QStackedWidget()
         open_button = QPushButton("Open")
         clear_button = QPushButton("Clear")
+        next_button = QPushButton(QIcon("next.svg"), "")
+        previous_button = QPushButton(QIcon("previous.svg"), "")
         preview_label = QLabel()
 
+        self.nav_widget = QWidget()
         self.size_widget = QWidget()
         self.size_checkbox = QCheckBox("Autosize")
         size_lineedit = QLineEdit(str(self.output_screen_size))
@@ -100,6 +124,7 @@ class Polo(QWidget):
         open_button.setToolTip("Choose a media file to display")
         clear_button.setToolTip("Clear the current media and turn off the display")
         size_lineedit.setInputMask("D0 \\i\\n")
+        self.nav_widget.setEnabled(False)
         self.media_preview_stack.addWidget(QSvgWidget("blank.svg"))
         self.media_preview_stack.addWidget(preview_label)
         self.display_widget.setScaledContents(True)
@@ -112,6 +137,8 @@ class Polo(QWidget):
         # Set up connections
         open_button.clicked.connect(self.choose_media)
         clear_button.clicked.connect(self.clear_media)
+        next_button.clicked.connect(lambda: self.advance_media(1))
+        previous_button.clicked.connect(lambda: self.advance_media(-1))
         size_lineedit.editingFinished.connect(self.size_changed)
         self.size_checkbox.stateChanged.connect(self.set_dimensions_visibility)
 
@@ -121,24 +148,35 @@ class Polo(QWidget):
         open_shortcut = makeShortcut("O")
         clear_shortcut = makeShortcut("C")
         close_shortcut = makeShortcut("Ctrl+Q")
+        self.next_shortcut = makeShortcut("N")
+        self.previous_shortcut = makeShortcut("P")
         self.dimensions_shortcut = makeShortcut("A")
+        self.next_shortcut.setEnabled(False)
+        self.previous_shortcut.setEnabled(False)
         self.dimensions_shortcut.setEnabled(False)
 
         open_shortcut.activated.connect(self.choose_media)
         clear_shortcut.activated.connect(self.clear_media)
         close_shortcut.activated.connect(self.close)
+        self.next_shortcut.activated.connect(lambda: self.advance_media(1))
+        self.previous_shortcut.activated.connect(lambda: self.advance_media(-1))
         self.dimensions_shortcut.activated.connect(self.size_checkbox.toggle)
 
         # Pack layouts
-        hbox = QHBoxLayout()
         vbox = QVBoxLayout()
+        hbox = QHBoxLayout()
+        nav_hbox = QHBoxLayout()
         size_hbox = QHBoxLayout()
+
+        nav_hbox.addWidget(previous_button)
+        nav_hbox.addWidget(next_button)
 
         size_hbox.addWidget(QLabel("Size:"))
         size_hbox.addWidget(size_lineedit)
 
         vbox.addWidget(open_button)
         vbox.addWidget(clear_button)
+        vbox.addWidget(self.nav_widget)
         vbox.addWidget(self.size_checkbox)
         vbox.addWidget(self.size_widget)
         vbox.addStretch()
@@ -150,7 +188,9 @@ class Polo(QWidget):
         hbox.setContentsMargins(20, 20, 20, 20)
         vbox.setSpacing(10)
 
+        nav_hbox.setContentsMargins(0, 0, 0, 0)
         size_hbox.setContentsMargins(0, 0, 0, 0)
+        self.nav_widget.setLayout(nav_hbox)
         self.size_widget.setLayout(size_hbox)
 
         # Create slave window
@@ -168,7 +208,6 @@ class Polo(QWidget):
         self.size_widget.hide()
 
         self.display_widget.setWindowTitle("Polo - Display")
-        self.display_widget.show()
 
         self.setLayout(hbox)
         self.setWindowTitle("Polo")
@@ -179,28 +218,61 @@ class Polo(QWidget):
         Open a dialog for the user to select a media file.
         """
         formatify = lambda formats: " *.".join([""] + formats).strip()
-        image_formats = ["bmp", "gif", "jpeg", "jpg", "png", "ppm"]
-        video_formats = ["avi", "mkv", "mov", "mp4", "mpg", "mpeg"]
         media_path = QFileDialog.getOpenFileName(self, "Select Media",
-                                                 filter="Images ({0});;".format(formatify(image_formats)) +
-                                                        "Videos ({0})".format(formatify(video_formats)))
+                                                 filter="Images ({0});;".format(formatify(self.image_fmts)) +
+                                                        "Videos ({0})".format(formatify(self.video_fmts)))
 
         if media_path[0]:
-            if any(media_path[0].endswith(codec) for codec in video_formats):
-                imageio.plugins.ffmpeg.download()
+            media_glob = str(Path(media_path[0]).parent / "*")
+            self.files = [f for f in glob.glob(media_glob)
+                          if self.get_fmt(f) in (self.image_fmts + self.video_fmts)]
+            self.files.sort(key=str.lower)
 
-                if type(self.media) is Video:
-                    self.stop()
+            self.current_file_index = self.files.index(media_path[0])
+            self.set_media()
 
-                self.media = imageio.get_reader(media_path[0], "ffmpeg")
-                self.qmedia = None
-            else:
-                self.media = Image.open(media_path[0])
-                self.qmedia = ImageQt(self.hologrify(self.media))
+    def set_media(self):
+        """
+        Sets the current media based on `self.current_file_index`.
+        """
+        media_path = self.files[self.current_file_index]
+        fmt = self.get_fmt(media_path)
 
-            self.size_checkbox.setEnabled(True)
-            self.dimensions_shortcut.setEnabled(True)
-            self.refresh()
+        # If the media is an image
+        if fmt in self.image_fmts:
+            self.media = Image.open(media_path)
+            self.qmedia = ImageQt(self.hologrify(self.media))
+        elif fmt in self.video_fmts: # If it's a video
+            imageio.plugins.ffmpeg.download()
+
+            if type(self.media) is Video:
+                self.stop()
+
+            self.media = imageio.get_reader(media_path, "ffmpeg")
+            self.qmedia = None
+
+        self.size_checkbox.setEnabled(True)
+        self.nav_widget.setEnabled(True)
+        self.next_shortcut.setEnabled(True)
+        self.previous_shortcut.setEnabled(True)
+        self.dimensions_shortcut.setEnabled(True)
+        self.refresh()
+
+    def get_fmt(self, path):
+        """
+        Returns the file extension of the given file path.
+        '/home/foo/bar.svg' -> 'svg'
+        """
+        return Path(path).suffix[1:]
+
+    def advance_media(self, step):
+        """
+        Move the current media through the list by `step` (can be positive or
+        negative).
+        """
+        self.current_file_index += step
+        self.current_file_index %= len(self.files)
+        self.set_media()
 
     def refresh(self):
         """
@@ -209,7 +281,8 @@ class Polo(QWidget):
         if issubclass(type(self.media), ImageFile.ImageFile):
             self.qmedia = ImageQt(self.hologrify(self.media))
             self.display_widget.setPixmap(QPixmap.fromImage(self.qmedia))
-            self.media_preview_stack.widget(1).setPixmap(QPixmap.fromImage(ImageQt(self.media)))
+            self.preview_qmedia = ImageQt(self.media)
+            self.media_preview_stack.widget(1).setPixmap(QPixmap.fromImage(self.preview_qmedia))
         elif type(self.media) is Video and not self.player_thread.is_alive():
             self.player_thread = threading.Thread(target=self.play)
             self.player_thread.start()
@@ -372,7 +445,10 @@ class Polo(QWidget):
         self.qmedia = None
         self.display_widget.clear()
         self.media_preview_stack.setCurrentIndex(0)
+        self.nav_widget.setEnabled(False)
         self.size_checkbox.setEnabled(False)
+        self.next_shortcut.setEnabled(False)
+        self.previous_shortcut.setEnabled(False)
         self.dimensions_shortcut.setEnabled(False)
 
     def center_widget(self, widget, screen):
@@ -397,6 +473,7 @@ class Polo(QWidget):
     def set_dimensions_visibility(self):
         self.refresh()
         self.size_widget.setVisible(not self.size_checkbox.isChecked())
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
